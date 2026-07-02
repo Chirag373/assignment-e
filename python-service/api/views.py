cb7a9809f51b405b4450a26401608ff8ebc9456a
Chirag <Ozachirag08@gmail.com>
2026-07-02 17:04:10 +0530

    Restore views.py and apply CSRF token forward fix

---
+++ python-service/api/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from .services import StudentReportService
+from .format import error_response
+
+class StudentReportAPIView(APIView):
+    def get(self, request, id):
+        headers = {}
+        if 'Authorization' in request.headers:
+            headers['Authorization'] = request.headers['Authorization']
+        if 'x-csrf-token' in request.headers:
+            headers['x-csrf-token'] = request.headers['x-csrf-token']
+        elif 'X-Csrf-Token' in request.headers:
+            headers['x-csrf-token'] = request.headers['X-Csrf-Token']
+            
+        cookies = request.COOKIES
+            
+        pdf_bytes, error, status_code = StudentReportService.generate_report(id, headers, cookies)
+        
+        if error:
+            return error_response(
+                error_message=error.get("error", "Failed to generate report"),
+                details=error.get("details"),
+                status_code=status_code
+            )
+            
+        response = HttpResponse(pdf_bytes, content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="student_{id}_report.pdf"'
+        return response
+
+
